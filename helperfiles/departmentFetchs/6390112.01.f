ce2c6f3feab620beebe25bd17083848df59f5f63
sahin <sahin.kasap@sirenbilisim.com.tr>
2022-03-03 05:14:23 +0300

    her şey güzel gidiyordu ancak sanırım cookie işleri bizi bozdu

---
+++ helperfiles/departmentFetchs/6390112.01.f
@@ -0,0 +1,21 @@
+fetch("https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "max-age=0",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "iframe",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "cookie": "_ga=GA1.3.313145970.1639841661; _APP_LOCALE=EN; phpSess_e7f8a2b66340bd43b00edc2a215826d5=Gq3P9i2Faf1TwCtYyKx8VJZ18s09xP9tuCOmrE1MMpM0MeX0aBGAW76332w9rd0gci9JMOB0W8ZaeBiQrcd2qLam1r2Vnpi49NfM8NzswtCnVJDouW3QVV4URVKsDKZF",
+    "Referer": "https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "submit_section=1&hidden_redir=Course_Info",
+  "method": "POST"
+});
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import * as homeController from '@/controller/home';
 import fetch from 'cross-fetch';
 import fs, { cp, write } from 'fs';
 import { Router } from 'express';
-import { Bolum, CourseInfo, Ders, Main, Prerequisite } from '@/types/general/Bolum';
+import { Bolum, CourseInfo, Ders, Main, Prerequisite, Section } from '@/types/general/Bolum';
 import xpath from 'xpath';
 import { DOMParser } from 'xmldom';
 import { dirname } from 'path';
@@ -66,9 +66,6 @@ router.get('/temp', async (req, res) => {
         isKibris: false,
       };
       main.bolumler.push(bolum);
-      if (main.bolumler.length % 10 == 0) {
-        writeResult(main);
-      }
       continue;
     }
     //else
@@ -96,23 +93,23 @@ router.get('/temp', async (req, res) => {
       console.log(totalCourses);
       let bolum: Bolum = {
         isInfoFound: true,
-        dersler: [],
+        dersler: await getDersler(doc, totalCourses, deptNum),
         name: name,
         code: deptNum,
         totalCourses: totalCourses,
         isKibris: name.includes('Kuzey K'),
       };
-      let dersler: Ders[] = await getDersler(doc, totalCourses, deptNum);
 
       //
-
-      if (main.bolumler.length % 10 == 0) {
+      main.bolumler.push(bolum);
+      
+    }
+    if (main.bolumler.length % 10 == 0) {
         writeResult(main);
-      }
     }
     w(deptNum, deptHtmlRes);
 
-    if (i > 11) break;
+    if (i > 11) break; // DEBUG
   }
   writeResult(main);
 
@@ -207,9 +204,34 @@ async function getCourseInfo(i: number, derslerDoc: Document):Promise<CourseInfo
         courseCode: 0,
         courseName: '',
         credit: '',
-        sectionlar: []
+        sectionlar: await getSectionBilgileri(),
     }
     return courseInfo;
+}
+
+async function getSectionBilgileri():Promise<Section[]>{
+    fetch("https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "max-age=0",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "iframe",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "cookie": "_ga=GA1.3.313145970.1639841661; _APP_LOCALE=EN; phpSess_e7f8a2b66340bd43b00edc2a215826d5=Gq3P9i2Faf1TwCtYyKx8VJZ18s09xP9tuCOmrE1MMpM0MeX0aBGAW76332w9rd0gci9JMOB0W8ZaeBiQrcd2qLam1r2Vnpi49NfM8NzswtCnVJDouW3QVV4URVKsDKZF",
+    "Referer": "https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "submit_section=1&hidden_redir=Course_Info",
+  "method": "POST"
+});
+    throw new Error();
 }
 
 function getPrerequisite(i: number, derslerDoc: Document):Promise<Prerequisite[]> {
