8595057cfaeeed8f3383f163d3cd0593ac375549
sahin <sahin.kasap@sirenbilisim.com.tr>
2022-03-03 05:57:26 +0300

    updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -218,10 +218,10 @@ async function getCourseInfo(
     'string(//*[@id="single_content"]/form/TABLE[3]/TR[5]/TD[1]/FONT/INPUT/@VALUE)';
   p('r->', r);
   let courseInfo: CourseInfo = {
-    department: '',
-    courseCode: 0,
-    courseName: '',
-    credit: '',
+    department: '',//TODO
+    courseCode: 0,//TODO
+    courseName: '',//TODO
+    credit: '',//TODO
     sectionlar: await getSectionBilgileri(sectionsDoc),
   };
   return courseInfo;
@@ -272,8 +272,8 @@ async function getSectionBilgileri(sectionsDoc: Document): Promise<Section[]> {
   
  
   let section: Section = {
-    instructor: '',
-    sectionNumber: 0,
+    instructor: '',//TODO
+    sectionNumber: 0,//TODO
     criteria: criterias
   }
   res.push(section);
@@ -286,9 +286,31 @@ function getPrerequisite(
   i: number,
   derslerDoc: Document,
 ): Promise<Prerequisite[]> {
-  throw new Error('Function not implemented.');
+
+  let response = fetch("https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php", {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "max-age=0",
+      "content-type": "application/x-www-form-urlencoded",
+      "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "sec-fetch-dest": "iframe",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "cookie": "_ga=GA1.3.313145970.1639841661; _APP_LOCALE=EN; phpSess_e7f8a2b66340bd43b00edc2a215826d5=cE4MDujnhX9nEPQJb25ki0LYjWvQoGUfNEI4SahEbB5DPEXRmSr14m0TKgTsyhKijeeJrQWCTOm6ldhdw0K0GxjrjYGyYaUJvSyb5L5kj0WMlEKeYgk9DIOrEI49h3kE",
+      "Referer": "https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "SubmitPrerequisite=Prerequisite&text_course_code=5700204&hidden_redir=Course_List",
+    "method": "POST"
+  });
+  
 }
-function getCriterias(doc: Document): Criteria[] {
+function getCriterias(doc: Document): Criteria[] { //DONE
   let criteriasBolumuX = '//*[@id="single_content"]/form/TABLE[3]/TR'
   let kacCriteria = xpath.select(criteriasBolumuX,doc).length-1;
   let res: Criteria[] = []
