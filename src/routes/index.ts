13caa63784f92f2858c32637b5a89a01ffa52d3b
sahin <sahin.kasap@sirenbilisim.com.tr>
2022-03-03 04:59:59 +0300

    updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import * as homeController from '@/controller/home';
 import fetch from 'cross-fetch';
 import fs, { cp, write } from 'fs';
 import { Router } from 'express';
-import { Bolum, Ders, Main } from '@/types/general/Bolum';
+import { Bolum, CourseInfo, Ders, Main, Prerequisite } from '@/types/general/Bolum';
 import xpath from 'xpath';
 import { DOMParser } from 'xmldom';
 import { dirname } from 'path';
@@ -135,66 +135,84 @@ function w(deptnum: number, text: any) {
 }
 
 async function getDersler(
-  doc: Document,
+  derslerDoc: Document,
   totalCourses: number,
   deptNum: number,
 ): Promise<Ders[]> {
+    let res:Ders[] = []
   for (let i = 0; i < totalCourses; i++) {
-    let courseCodeXpath = `//*[@id="single_content"]/form/TABLE[4]/TR[${
-      i + 2
-    }]/TD[2]/FONT/text()`;
-    let courseCode = xpath.select(courseCodeXpath, doc).toString();
+    let courseInfo = await getCourseInfo(i, derslerDoc);
+    let prerequisite = await getPrerequisite(i,derslerDoc);
+    let ders:Ders={
+        courseInfo: courseInfo,
+        prerequisite: prerequisite
+    }
+    res.push(ders);
+  }
+  return [];
+}
+
+
+async function getCourseInfo(i: number, derslerDoc: Document):Promise<CourseInfo> {
+    let courseCodeXpath = `//*[@id="single_content"]/form/TABLE[4]/TR[${i + 2}]/TD[2]/FONT/text()`;
+    let courseCode = xpath.select(courseCodeXpath, derslerDoc).toString();
 
     p('courseCode', courseCode);
-    let response = await fetch(
-      'https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php',
-      {
-        headers: {
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-          'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-          'cache-control': 'max-age=0',
-          'content-type': 'application/x-www-form-urlencoded',
-          'sec-ch-ua':
-            '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"Windows"',
-          'sec-fetch-dest': 'iframe',
-          'sec-fetch-mode': 'navigate',
-          'sec-fetch-site': 'same-origin',
-          'sec-fetch-user': '?1',
-          'upgrade-insecure-requests': '1',
-          cookie:
-            '_ga=GA1.3.313145970.1639841661; _APP_LOCALE=EN; phpSess_e7f8a2b66340bd43b00edc2a215826d5=Me2CWS88sguBhTLhgZmeBw69YjIp9oHpmUTw5zBNk87W8eAGEvbeBfUwzEYJxhUHrtgm6AdBn7P7JXRrl2ylTbmuM7TJqSBP11wOO6W1V5qkApNWvsYNBcqVDho6jg42',
-          Referer:
-            'https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php',
-          'Referrer-Policy': 'strict-origin-when-cross-origin',
-        },
-        body: `SubmitCourseInfo=Course+Info&text_course_code=${courseCode}&hidden_redir=Course_List`,
-        method: 'POST',
-      },
+    let courseInfoFetch = await fetch(
+        'https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php',
+        {
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                'content-type': 'application/x-www-form-urlencoded',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'iframe',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                cookie: '_ga=GA1.3.313145970.1639841661; _APP_LOCALE=EN; phpSess_e7f8a2b66340bd43b00edc2a215826d5=Me2CWS88sguBhTLhgZmeBw69YjIp9oHpmUTw5zBNk87W8eAGEvbeBfUwzEYJxhUHrtgm6AdBn7P7JXRrl2ylTbmuM7TJqSBP11wOO6W1V5qkApNWvsYNBcqVDho6jg42',
+                Referer: 'https://oibs2.metu.edu.tr//View_Program_Course_Details_64/main.php',
+                'Referrer-Policy': 'strict-origin-when-cross-origin',
+            },
+            body: `SubmitCourseInfo=Course+Info&text_course_code=${courseCode}&hidden_redir=Course_List`,
+            method: 'POST',
+        }
     );
     // console.log('responz');
     // console.log(response);
-    let sectionsHtml = await response.text();
+    let sectionsHtml = await courseInfoFetch.text();
     fs.writeFileSync(`temp/${courseCode}.html`, sectionsHtml);
     let senctionsDoc = new DOMParser({
         locator: {},
         errorHandler: {
-          warning: function (w) {},
-          error: function (e) {},
-          fatalError: function (e) {
-            console.error(e);
-          },
+            warning: function (w) { },
+            error: function (e) { },
+            fatalError: function (e) {
+                console.error(e);
+            },
         },
-      }).parseFromString(sectionsHtml);
+    }).parseFromString(sectionsHtml);
 
-    let tempXpath = 'string(//*[@id="single_content"]/form/TABLE[3]/TR[3]/TD[1]/FONT/INPUT/@VALUE)'
-    let t1 ='//*[@id="single_content"]/form/table[3]/tbody/tr[3]/td[1]'
-    let chrome = '//*[@id="single_content"]/form/table[3]/tbody/tr[3]/td[1]/font/input'
-    let r = xpath.select(tempXpath,senctionsDoc).toString();
-    p("r->",r);
-    throw new Error('unimplement');
-  }
-  return [];
+    let ilkSectionIcin = 'string(//*[@id="single_content"]/form/TABLE[3]/TR[3]/TD[1]/FONT/INPUT/@VALUE)';
+    let r = xpath.select(ilkSectionIcin, senctionsDoc).toString();
+    let ikiChrome = '//*[@id="single_content"]/form/table[3]/tbody/tr[5]/td[1]/font/input';
+    let ikiReel = 'string(//*[@id="single_content"]/form/TABLE[3]/TR[5]/TD[1]/FONT/INPUT/@VALUE)';
+    p("r->", r);
+    let courseInfo: CourseInfo = {
+        department: '',
+        courseCode: 0,
+        courseName: '',
+        credit: '',
+        sectionlar: []
+    }
+    return courseInfo;
 }
+
+function getPrerequisite(i: number, derslerDoc: Document):Promise<Prerequisite[]> {
+    throw new Error('Function not implemented.');
+}
+
